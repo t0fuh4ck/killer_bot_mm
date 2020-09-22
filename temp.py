068c5230dfec4bd697c322c97bc582e826b5ea35
t0fuh4ck <thforadream@gmail.com>
2020-09-22 15:15:04 +0300

    lol sdvig

--- temp.py
+++ temp.py
@@ -8,27 +8,27 @@ class BotHandler:
         self.api_url = "https://api.telegram.org/bot1363975687:AAEMW-BOdnqlMc8CVkrEeXYnRklBKV9o0kU/".format(token)
         #"https://...bot{}/".format(token)
 
-def get_updates(self, offset=None, timeout=30):
-    method = 'getUpdates'
-    params = {'timeout': timeout, 'offset': offset}
-    resp = requests.get(self.api_url + method, params)
-    result_json = resp.json()['result']
-    return result_json
+	def get_updates(self, offset=None, timeout=30):
+		method = 'getUpdates'
+		params = {'timeout': timeout, 'offset': offset}
+		resp = requests.get(self.api_url + method, params)
+		result_json = resp.json()['result']
+		return result_json
 
-def send_message(self, chat_id, text):
-    params = {'chat_id': chat_id, 'text': text}
-    method = 'sendMessage'
-    resp = requests.post(self.api_url + method, params)
-    return resp
+	def send_message(self, chat_id, text):
+        params = {'chat_id': chat_id, 'text': text}
+        method = 'sendMessage'
+        resp = requests.post(self.api_url + method, params)
+        return resp
 
-def get_last_update(self):
-    get_result = self.get_updates()
+	def get_last_update(self):
+        get_result = self.get_updates()
     
-    if len(get_result) > 0:
-        last_update = get_result[-1]
+        if len(get_result) > 0:
+            last_update = get_result[-1]
         
-    else:
-        last_update = get_result[len(get_result)]
+        else:
+            last_update = get_result[len(get_result)]
         
         
         return last_update
